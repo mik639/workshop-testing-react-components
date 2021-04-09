2b9f9aaa9b01a13f2ab78ed7e697300c0ba25cf3
Nikolaevskiy Mikhail <mik639inc@gmail.com>
2021-04-09 13:01:50 +0400

    Test cases teplates

--- src/components/splitIt/splitIt.test.js
+++ src/components/splitIt/splitIt.test.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {render, screen} from '@testing-library/react';
 
 import { SplitIt } from "./splitIt";
 
-configure({ adapter: new Adapter(), disableLifecycleMethods: true });
-
 describe("<SplitIt/> in base product info", () => {
   let props;
-  let wrapper;
   beforeEach(() => {
     props = {
       total: 150,
@@ -18,63 +14,36 @@ describe("<SplitIt/> in base product info", () => {
       getData: jest.fn(),
       isLoaded: true
     };
-    wrapper = shallow(<SplitIt {...props} />);
     window.dataLayer = [];
   });
 
-  describe("componentDidMount method", () => {
-    it("should call props getData", () => {
-      expect(props.getData).not.toHaveBeenCalled();
-
-      wrapper.instance().componentDidMount();
-      expect(props.getData).toHaveBeenCalled();
-    });
-  });
-
-  describe("openPopup method", () => {
-    it("should change popupVisible in state in true", () => {
-      wrapper.setState({ popupVisible: false });
-      wrapper.instance().openPopup();
-      expect(wrapper.state("popupVisible")).toBe(true);
-    });
-
-    it("should send analytics event", () => {
-      const spy = jest.spyOn(window.dataLayer, "push");
-      expect(spy).not.toHaveBeenCalled();
-      wrapper.instance().openPopup();
-      expect(spy).toHaveBeenCalledWith({
-        event: "PDPInteraction",
-        eventAction: "Product Details",
-        eventCategory: "PDP - D",
-        eventLabel: "Learn More - Split It"
-      });
-    });
-  });
-
-  describe("closePopup method", () => {
-    it("should change popupVisible in state in false", () => {
-      wrapper.setState({ popupVisible: true });
-
-      wrapper.instance().closePopup();
-      expect(wrapper.state("popupVisible")).toBe(false);
-    });
+  it('should call getData prop on mount', () => {
+    // Давайте отрендерим компонент и проверим что проп getData был вызван
   });
 
   it("should render text and button if total > over", () => {
-    wrapper.setProps({ total: 150, over: 100 });
-    expect(wrapper.contains("Or $ 50.00 x 3")).toBe(true);
-    expect(wrapper.find('[title="learn more"]')).toHaveLength(1);
+    // Проверим что при условии, что сумма заказа больше минимального порога
+    // компонент рендерит нужный текст и кнопку "learn more"
   });
 
   it("should NOT render text and button if total < over", () => {
-    wrapper.setProps({ total: 100, over: 120 });
-
-    expect(wrapper.isEmptyRender()).toBe(true);
+    // Проверим что при условии, что сумма заказа меньше минимального порога
+    // компонент ничего не рендерит
   });
 
   it("should NOT render text and button if data is loading", () => {
-    wrapper.setProps({ total: 150, over: 100, isLoaded: false });
+    // Проверим что компонент ничего не рендерит пока данные не загружены
+  });
 
-    expect(wrapper.isEmptyRender()).toBe(true);
+  test('learn more popup', () => {
+    // Теперь давайте опишем взаимодействие пользователя с попапом
+
+    // 1. Найдем кнопку "learn more", и кликнем по ней
+    // 1.1. Проверим что была отправлена аналитика
+    // 1.2. Проверим что попап отобразился, для этого проверим что на экране появился его заголовок
+    //      с рассчетом суммы платежа (Your monthly payment $50 ($50 × 3 = $150))
+    
+    // 2. Найдем в попапе кнопку "Close", и кликнем по ней.
+    // 2.1 Проверим что попап больше не отображается на экране.
   });
 });
